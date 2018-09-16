e674bee4c59df84fedb74f4ddb24ae166507593d
Orestis Roditis-Koutsantonis <orestarod@gmail.com>
2018-09-16 17:26:56 +0300

    more readable and clean format

--- LISP/peuler.lisp
+++ LISP/peuler.lisp
@@ -1,5 +1,6 @@
 ;; Problem 1
 ;; Takes a limit number, and a list of numbers. Returns the sum of all the numbers from 1 up to and including limit-1, which are divided by one of the argument numbers
+;; anums just concatenates the needed first number with the list of optional rest of the numbers.
 (defun sum_mults (limit fst &rest nums &aux (anums (cons fst nums)))
     (loop for x from 1 to (- limit 1) 
         sum (if 
@@ -12,12 +13,11 @@
 
 ;; Problem 2
 ;; Takes a limit and a test function, and produces the sum of all fibonacci numbers up to limit non-inclusive that satisfy the test given - test is a function
-(defun fib_sum_test (limit test &aux (p2 0) (p1 1))
-    (loop 
+(defun fib_sum_test (limit test)
+    (loop
+        for p2 = 0 then p1
+        and p1 = 1 then (+ p1 p2)
         while (< p2 limit)
-        do
-         (progn (setf p1 (+ p1 p2))
-                (setf p2 (- p1 p2)))
         sum (if (funcall test p2) p2 0)))
 
 (defun fib_sum_even (limit) (fib_sum_test limit #'evenp))
