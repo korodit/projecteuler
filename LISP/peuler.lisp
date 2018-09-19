a9f17721c95616d8ad4128df30f8ede0b7949f76
Orestis Roditis-Koutsantonis <orestarod@gmail.com>
2018-09-19 21:12:56 +0300

    added tags to problems

--- LISP/peuler.lisp
+++ LISP/peuler.lisp
@@ -1,6 +1,7 @@
 ;; General Utilities
 
 ;; Range, limit non-inclusive if ascending sequence, inclusive if descending
+;; #range #numbers
 (defun range (start &optional limit &key (stp 1))
     (if (not (numberp start)) (error "Non-number input for range"))
     (if (not (numberp stp)) (error "Non-number input for range"))
@@ -14,6 +15,7 @@
             collect i)))
 
 ;; Prime list up to limit non-inclusive in order
+;; #prime
 (defun prime_list (limit)
     (loop
         for nums = (range 2 limit) then (delete-if #'(lambda (x) (= 0 (rem x (car nums)))) (cdr nums))
@@ -21,15 +23,18 @@
         collect (car nums)))
 
 ;; Finds all prime factors of x and returns them as a list
+;; #prime #factor
 (defun prime_factors_slow (x)
     (delete-if-not #'(lambda (y) (= 0 (rem x y))) (prime_list (floor (sqrt x)))))
 
 ;; Takes a number to be divided and a divisor of it and gives the power of the divisor that exists in the divided number
 ;; plus the divided number if the divisor is removed from it
+;; #factor #divisor #divided #power
 (defun times_divided (divided divisor already)
     (if (/= 0 (rem divided divisor)) (values already divided) (times_divided (/ divided divisor) divisor (+ already 1))))
 
 ;; Result is in the form ((factor1 power1) (factor2 power2) ...)
+;; #prime #factor #power
 (defun prime_factors_powers (ceil &aux pow)
     (if (not (and (integerp ceil) (plusp ceil))) (error "Non positive integer given to function prime_factors"))
     (loop
@@ -42,6 +47,7 @@
 ;; Problem #1
 ;; Takes a limit number, and a list of numbers. Returns the sum of all the numbers from 1 up to and including limit-1, which are divided by one of the argument numbers
 ;; anums just concatenates the needed first number with the list of optional rest of the numbers.
+;; #sum #multiple
 (defun sum_mults (limit fst &rest nums &aux (anums (cons fst nums)))
     (loop for x from 1 to (- limit 1) 
         sum (if 
@@ -54,6 +60,7 @@
 
 ;; Problem #2
 ;; Takes a limit and a test function, and produces the sum of all fibonacci numbers up to limit non-inclusive that satisfy the test given - test is a function
+;; #fibonacci #sequence #sum
 (defun fib_sum_test (limit test)
     (loop
         for p2 = 0 then p1
@@ -67,6 +74,7 @@
 
 ;; Problem #3
 ;; Takes x and finds its largest prime factor
+;; #largest #max #prime #factor
 (defun largest_prime_factor_slow (x)
     (let ((candidates (prime_factors_powers x)))
             ;; If no prime factors other than itself, the candidates list is empty!
